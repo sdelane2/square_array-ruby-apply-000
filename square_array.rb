3a881ef64aa7db13580f6b7fb70e898e2b9c0364
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 23:40:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
-def square_array(n)
-  n = [1, 2, 3]
-  n.each do |n|
-   puts "n*n"
+array = [1, 2, 3]
+def square_array(array)
+  array.each do |number|
+    puts "#{number} * #{number}"
   end
 end
