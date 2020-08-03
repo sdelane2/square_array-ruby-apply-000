e2a283011e4242dd689bfe2130b9158a8f13eef0
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-03 00:00:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,7 @@
 array = [1, 2, 3]
+results_array = []
 def square_array(array)
   array.each do |number|
     puts number**2
-  end
-end
-array = [1, 2, 3]
-def square_array(array)
-  array.collect do |number|
-    puts number**2
-    number**2
   end
 end
